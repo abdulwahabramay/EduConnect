d3bbcac97141db2c58125b61df742a9fbb2ae556
abdumwahabramay <abdulwahabramay4@gmail.com>
2024-10-07 20:03:48 +0500

    added git ignore file

--- django_project/resources/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Resource
-
-class ResourceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Resource
-        fields = ['id', 'file', 'category', 'tags', 'courses', 'uploaded_by']
